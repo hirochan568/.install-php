662ca30675bbe5ddf930127567756368a9513749
TAKAHIRO <takahiro1109tm568@gmail.com>
2021-07-08 23:40:59 +0900

    set-invironment4

--- main.php
+++ main.php
@@ -1,3 +1,18 @@
 <?php
-echo 'Hello hiro!'. PHP_EOL;
-echo 'Hi hiro' . PHP_EOL;
+// ↓変数
+$name = 'hiro';
+
+
+// echo 'Hello hiro!'.PHP_EOL;
+// echo 'Hi hiro' .PHP_EOL;
+
+echo 'Hello '. $name . PHP_EOL;
+echo 'Hi '. $name .PHP_EOL;
+
+//コメント1
+#コメント2
+/*
+コメント3
+改行してもOK
+終わり
+*/
